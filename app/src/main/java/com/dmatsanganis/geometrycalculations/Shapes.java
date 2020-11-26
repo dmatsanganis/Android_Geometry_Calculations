b395542653857d03fdbf18da570ba715f2ef8f46
Dimitris Matsanganis <jimmatsa@hotmail.gr>
2020-11-26 13:14:35 +0200

    Add Shapes.java

---
+++ app/src/main/java/com/dmatsanganis/geometrycalculations/Shapes.java
@@ -0,0 +1,14 @@
+package com.dmatsanganis.geometrycalculations;
+
+/**
+ * Ignore this Class, This class is called an Enum.
+ * Enum(s) are a special type of class
+ * in Java that have special characteristics.
+ */
+public enum Shapes {
+    Rectangle,
+    Box,
+    Circle,
+    Sphere,
+    Triangle
+}
